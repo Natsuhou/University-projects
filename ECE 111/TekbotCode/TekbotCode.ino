51587c841aca8f309913002bf4143a374970549e
Matthew <32581314+Shogatsu@users.noreply.github.com>
2019-11-12 23:22:08 -0800

    Add files via upload

--- ECE 111/TekbotCode/TekbotCode.ino
+++ ECE 111/TekbotCode/TekbotCode.ino
@@ -4,37 +4,43 @@
  * Group 1: Andrew, Matthew and Lucy
  * Date: 10/28/19
  */
-#define R1_MOTOR 2 //MOTOR 1 D2
-#define R2_MOTOR 3 //MOTOR 1 D4
-#define L1_MOTOR 4 //MOTOR 2 D5
-#define L2_MOTOR 5 //MOTOR 2 D6
-#define B_RIGHT 7 //Right bumper D7
-#define B_LEFT 6 //Left bumper D8
-#define KILLSWITCH 8 //Killswitch D9
-#define PWM_M1 3 //PWM for motor 1
-#define PWM_M2 3 //PWM for motor 2
-#define PWM_SPEED 130 //Speed
-/*
- * Note:
- * 2 PWM pins are still needed so use breadboard
- * Reserve D3 for PWM!
- */
+#define R1_MOTOR 7//MOTOR 1 D2
+#define R2_MOTOR 6 //MOTOR 1 D4
+#define L1_MOTOR 8 //MOTOR 2 D5
+#define L2_MOTOR 9 //MOTOR 2 D6
+#define B_RIGHT 4 //Right bumper D4
+#define B_LEFT 2 //Left bumper D2
+#define PWM_L 5 //PWM for motor 1
+#define PWM_R 3 //PWM for motor 2
+#define PWM_SPEED 250 //Speed
 
-//Declare global variables
+
+//Use enable pins on DNE
+
+//Variables used for left bumper right bumper
 int B_RIGHT2 = 0;
 int B_LEFT2 = 0;
 
 void setup() {
-  //Right motor
+  //Setting the pinmode for the right motors
   pinMode(R1_MOTOR,OUTPUT);
   pinMode(R2_MOTOR,OUTPUT);
-  //Left motor
+  //Setting the pinmode for the left motors
   pinMode(L1_MOTOR,OUTPUT);
   pinMode(L2_MOTOR,OUTPUT);
-  //Right and left bumpers    
-  //Pull              
+  /*       
+   * The pinmode for the right and the left bumper are set to INPUT_PULLUP
+   * so we don't get environmental interference with our bumper
+   */
   pinMode(B_RIGHT,INPUT_PULLUP);
   pinMode(B_LEFT,INPUT_PULLUP);
+
+  pinMode(PWM_R,OUTPUT);
+  pinMode(PWM_L,OUTPUT);
+
+  digitalWrite(PWM_L,PWM_SPEED);
+  digitalWrite(PWM_R,PWM_SPEED);
+  Serial.begin(9600);
 }
 
 void loop() { 
@@ -56,13 +62,21 @@ void loop() {
    */
    //Assuming both bumpers are connected to ground
   if (B_RIGHT2 == 0 && B_LEFT2 == 0) { 
+    //Method called from bothBump function
+    Serial.println("BOTH BUMPERS ARE BEING CALLED");
     bothBump();
   } else if (B_RIGHT2 == 0) {
+    //Method called from rightBump function
+    Serial.println("B_RIGHT2 is being called");
     rightBump();
   } else if (B_LEFT2 == 0) {
+    //Method called from leftBump function
+    Serial.println("B_LEFT2 is being called");
     leftBump();
   } else {
-    moveForward(250);
+    //If none of these are pressed, just move forward
+    moveForward();
+    Serial.println("Nothing being pressed");
   }
 }
 
@@ -74,66 +88,62 @@ void loop() {
  * Next objective:
  * Find what "180 degrees" is in a turn in MS
  */
+//Function Goal: Tekbok will back up, turn right and continue forward
 void leftBump() {
-  //Tekbok will back up, turn right and continue forward
   //Back up
-  backUp(100);
+  backUp();
+  delay(1000);
   //Turn right
+  digitalWrite(R1_MOTOR,HIGH);
+  digitalWrite(R2_MOTOR,LOW);
+  digitalWrite(L1_MOTOR,LOW);
+  digitalWrite(L2_MOTOR,LOW);
+  delay(1060);
+
+  //Go forward
+  moveForward();
+}
+//Function Goal: Tekbot will backup, turn left and continue forward
+void rightBump() {
+  //Back up
+  backUp();
+  delay(1000);
+  //Turn left
   digitalWrite(R1_MOTOR,LOW);
   digitalWrite(R2_MOTOR,LOW);
   digitalWrite(L1_MOTOR,HIGH);
   digitalWrite(L2_MOTOR,LOW);
-  delay(100);
+  delay(1100);
 
   //Go forward
-  moveForward(250);
+  moveForward();
 }
-
-void rightBump() {
-  //Tekbot will backup, turn left and continue forward
-  //Back up
-   backUp(100);
-  delay(100);
-  //Turn left
-  digitalWrite(R1_MOTOR,HIGH);
-  digitalWrite(R2_MOTOR,HIGH);
-  digitalWrite(L1_MOTOR,HIGH);
-  digitalWrite(L2_MOTOR,LOW);
-  delay(100);
-
-  //Go forward
-  moveForward(250);
-}
-
+ //Function Goal: Tekbot will backup, turn 180 and continue forward
 void bothBump() {
-  //Tekbot will backup, turn 180 and continue forward
-  
   //Backup
-  backUp(100);
+  backUp();
+  delay(1000);
   //Turn 180
   digitalWrite(R1_MOTOR,HIGH);
   digitalWrite(R2_MOTOR,LOW);
   digitalWrite(L1_MOTOR,LOW);
   digitalWrite(L2_MOTOR,LOW);
-  delay(500);
+  //1900 good 90
+  delay(1930);
   //Move forward
-  moveForward(250);
+  moveForward();
 }
-
-
-//Functions called in the bump functions
-void backUp(int delayTime) {
+//Backup function used in other functions above
+void backUp() {
   digitalWrite(R1_MOTOR,LOW);
   digitalWrite(R2_MOTOR,HIGH);
   digitalWrite(L1_MOTOR,LOW);
   digitalWrite(L2_MOTOR,HIGH);
-  delay(delayTime);
 }
-
-void moveForward(int delayTime) {
+//moveForward function used in other functions above
+void moveForward() {
   digitalWrite(R1_MOTOR,HIGH);
   digitalWrite(R2_MOTOR,LOW);
   digitalWrite(L1_MOTOR,HIGH);
   digitalWrite(L2_MOTOR,LOW);
-  delay(delayTime);
 }
